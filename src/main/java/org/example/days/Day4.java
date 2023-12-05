cb4175b74f1957a1d15139fe85cd3832f5433b2d
Thibault MONEGIER <thibault.monegierdusorbier@ext.bpifrance.fr>
2023-12-05 22:49:26 +0100

    feat: add solution for day 4

--- src/main/java/org/example/days/Day4.java
+++ src/main/java/org/example/days/Day4.java
@@ -1,11 +1,29 @@
 package org.example.days;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.regex.Pattern;
 
 public class Day4 {
 
-    public static long solvePart1(Path path) throws IOException {
-        return 0;
+    public static final Pattern ASSIGNMENT_PATTERN = Pattern.compile("(?<minA>\\d+)-(?<maxA>\\d+),(?<minB>\\d+)-(?<maxB>\\d+)");
+
+    public static long solve(Path path, boolean isPart2) throws IOException {
+        return Files.lines(path).filter(
+           line -> {
+               var assignmentMatcher = ASSIGNMENT_PATTERN.matcher(line);
+               if(assignmentMatcher.find()) {
+                   int minA = Integer.parseInt(assignmentMatcher.group("minA"));
+                   int maxA = Integer.parseInt(assignmentMatcher.group("maxA"));
+                   int minB = Integer.parseInt(assignmentMatcher.group("minB"));
+                   int maxB = Integer.parseInt(assignmentMatcher.group("maxB"));
+
+                   return (isPart2 && Math.max(minA, minB) <= Math.min(maxA, maxB) ) ||
+                           (!isPart2 && (minA <= minB && maxA >= maxB) || (minB<= minA && maxB >= maxA));
+               }
+               return false;
+           }
+       ).count();
     }
 }
